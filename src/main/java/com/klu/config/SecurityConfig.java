206e25ae85528e5a50ae12407a5bc82994bc9f1e
VimalKumar099 <2400032827@kluniversity.in>
2026-04-02 09:38:02 +0530

    Add new allowed origin to CORS configuration
    Updated CORS configuration to include a new allowed origin.

--- src/main/java/com/klu/config/SecurityConfig.java
+++ src/main/java/com/klu/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-            "https://ecommerce-frontend-production-046e.up.railway.app"
+            "https://frontenddeploy-production.up.railway.app/"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
